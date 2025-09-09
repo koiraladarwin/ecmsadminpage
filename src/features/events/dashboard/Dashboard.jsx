0a51d80abf09568a4d4e3be88a3772215fae98df
Aarona KC <aaronakc99@gmail.com>
2025-09-09 18:06:30 +0545

    refactor: rearranged the taskbar and navbar layout again

--- src/features/events/dashboard/Dashboard.jsx
+++ src/features/events/dashboard/Dashboard.jsx
@@ -5,11 +5,13 @@ import TaskBar from './components/TaskBar'
 export default function Dashboard() {
   return (
     <>
-      <div className='flex flex-col min-h-screen'>
-        <NavBar />
+      <div className='flex min-h-screen'>
         <div className='flex flex-1'>
           <TaskBar />
-          <div className='flex-1 p-6 bg-gray-100 overflow-auto'>
+        </div>
+        <div class='w-full'>
+          <NavBar />
+          <div className='flex-1 p-6 overflow-auto'>
             <h1 className='text-2xl font-bold'>Dashboard</h1>
           </div>
         </div>
