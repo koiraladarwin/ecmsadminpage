84e1398207a36e2d375eee39c1d0ddafa6738269
Aarona KC <aaronakc99@gmail.com>
2025-09-09 17:38:09 +0545

    feat(dashboard):add simple the taskbar layout

--- src/features/events/dashboard/Dashboard.jsx
+++ src/features/events/dashboard/Dashboard.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import NavBar from './components/NavBar'
+import TaskBar from './components/TaskBar'
 
 export default function Dashboard() {
   return (
     <>
       <NavBar />
+      <TaskBar />
     </>
   )
 }
--- src/features/events/dashboard/components/TaskBar.jsx
+++ src/features/events/dashboard/components/TaskBar.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const TaskBar = () => {
   return (
-    <div>TaskBar</div>
+    <>
+      <div className='w-64 h-screen bg-slate-800 flex flex-col items-center'>
+        <div className="w-12 h-12 rounded-full bg-orange-600 flex justify-center items-center mb-6 mt-6">
+          <h1 className="text-white font-bold">U</h1>
+        </div>
+
+        <div className='flex flex-col space-y-10'>
+          <div><Link className='block w-full px-4 py-2 rounded text-white  hover:bg-slate-200 hover:text-orange-400'>Home</Link></div>
+          <div><Link className='block w-full px-4 py-2 rounded text-white hover:bg-slate-200 hover:text-orange-400'>Event</Link></div>
+          <div><Link className='block w-full px-4 py-2 rounded text-white hover:bg-slate-200 hover:text-orange-400'>Import</Link></div>
+        </div>
+
+      </div>
+    </>
   )
 }
 
