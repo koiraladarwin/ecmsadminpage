2ef26002ecce1dca135f705cfd6b99f09e77fe1a
Aarona KC <aaronakc99@gmail.com>
2025-09-15 21:55:48 +0545

    add validation to attendee form using reactForm hook

--- src/features/dashboard/people/components/form/AddAttendeeForm.jsx
+++ src/features/dashboard/people/components/form/AddAttendeeForm.jsx
@@ -1,12 +1,17 @@
 import React, { useState } from 'react'
 import NormalBtn from '../NormalBtn'
+import { useForm } from 'react-hook-form'
 
 function AddAttendeeForm({ onClick, form }) {
-  const [tag, setTag] = useState('')
+  const { register, handleSubmit, formState: { errors } } = useForm()
+
+  const onSubmit = (data) => {
+    console.log(data)
+  }
 
   return (
     <div className="w-full flex justify-center items-center py-10 pb-15">
-      <div className="w-full bg-white p-6 sm:p-10 rounded-lg shadow-md border-[1.4px] border-bg-sidebar-bg flex flex-col gap-6">
+      <form onSubmit={handleSubmit(onSubmit)} className="w-full bg-white p-6 sm:p-10 rounded-lg shadow-md border-[1.4px] border-bg-sidebar-bg flex flex-col gap-6">
 
         {/* Attendee Type */}
         <div className="flex flex-col md:flex-row gap-6 justify-end">
@@ -37,7 +42,17 @@ function AddAttendeeForm({ onClick, form }) {
           <label className="font-bold text-sidebar-bg">
             Full Name <span className="text-sidebar-bg">*</span>
           </label>
-          <input type="text" className="border rounded-sm p-2 mt-1 w-full outline-none" />
+          <input type="text" className="border rounded-sm p-2 mt-1 w-full outline-none"
+            {...register("fullName", {
+              required: "Full Name is required",
+              pattern: {
+                value: /^[A-Za-z ]+$/,
+                message: "full name contains only alphabets"
+                ,
+              },
+            })}
+          />
+          {errors.fullName && <p className="text-red-500">{errors.fullName.message}</p>}
         </div>
 
         {/* Category, Tag, Upload Photo */}
@@ -59,8 +74,7 @@ function AddAttendeeForm({ onClick, form }) {
               Choose Tag <span className="text-sidebar-bg">*</span>
             </label>
             <select
-              value={tag}
-              onChange={(e) => setTag(e.target.value)}
+              {...register("tag", { required: "Tag is required" })}
               className="border rounded-sm p-2 mt-1 w-full focus:outline-none"
             >
               <option value="">Select tag</option>
@@ -68,6 +82,7 @@ function AddAttendeeForm({ onClick, form }) {
               <option value="tag-2">TAG2</option>
               <option value="tag-3">TAG3</option>
             </select>
+            {errors.tag && <p className="text-red-500 text-sm mt-1">{errors.tag.message}</p>}
           </div>
 
           {/* upload photo */}
@@ -76,6 +91,7 @@ function AddAttendeeForm({ onClick, form }) {
             <input
               type="file"
               className="border rounded-sm  mt-1 w-full outline-none"
+              {...register("photo")}
             />
           </div>
         </div>
@@ -85,13 +101,17 @@ function AddAttendeeForm({ onClick, form }) {
           <label className="font-bold text-sidebar-bg">
             Company / Organization Name <span className="text-sidebar-bg">*</span>
           </label>
-          <input type="text" className="border rounded-sm p-2 mt-1 w-full outline-none" />
+          <input type="text" className="border rounded-sm p-2 mt-1 w-full outline-none"
+            {...register("company", { required: "Company/Organization name is required" })}
+          />
+          {errors.company && <p className="text-red-500 text-sm mt-1">{errors.company.message}</p>}
         </div>
 
         {/* Address */}
         <div className="flex flex-col">
           <label className="font-bold text-sidebar-bg">Address</label>
-          <input type="text" className="border rounded-sm p-2 mt-1 w-full outline-none" />
+          <input type="text" className="border rounded-sm p-2 mt-1 w-full outline-none"
+            {...register("address")} />
         </div>
 
         {/* Designation & Mobile */}
@@ -99,7 +119,9 @@ function AddAttendeeForm({ onClick, form }) {
         <div className="flex flex-col sm:flex-row gap-4 sm:gap-6">
           <div className="flex flex-col flex-1">
             <label className="font-bold text-sidebar-bg">Designation</label>
-            <input type="text" className="border rounded-sm p-2 mt-1 w-full ouline-none" />
+            <input type="text" className="border rounded-sm p-2 mt-1 w-full ouline-none"
+              {...register("designation")}
+            />
           </div>
 
           {/* mobile */}
@@ -107,7 +129,16 @@ function AddAttendeeForm({ onClick, form }) {
             <label className="font-bold text-sidebar-bg">
               Mobile Number <span className="text-sidebar-bg">*</span>
             </label>
-            <input type="text" className="border rounded-sm p-2 mt-1 w-full outline-none" />
+            <input type="text" className="border rounded-sm p-2 mt-1 w-full outline-none"
+              {...register("mobile", {
+                required: "Mobile number is required",
+                pattern: {
+                  value: /^[0-9]{10}$/,
+                  message: "should be number and 10 digits",
+                },
+              })}
+            />
+            {errors.mobile && <p className="text-red-500">{errors.mobile.message}</p>}
           </div>
         </div>
 
@@ -116,7 +147,15 @@ function AddAttendeeForm({ onClick, form }) {
           <label className="font-bold text-sidebar-bg">
             Email Address <span className="text-sidebar-bg">*</span>
           </label>
-          <input type="email" className="border rounded-sm p-2 mt-1 w-full outline-none" />
+          <input type="email" className="border rounded-sm p-2 mt-1 w-full outline-none"
+            {...register("email", {
+              required: "Email is required",
+              pattern: {
+                value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                message: "Invalid email format",
+              },
+            })} />
+          {errors.email && <p className="text-red-500">{errors.email.message}</p>}
         </div>
 
         {/* btns */}
@@ -124,7 +163,7 @@ function AddAttendeeForm({ onClick, form }) {
           <NormalBtn text="Save" type='primary' />
           <NormalBtn text="Save & Add New" type='primary' />
         </div>
-      </div>
+      </form>
     </div>
   )
 }
