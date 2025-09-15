3de25d7a91c362a398f47aa8a59e5d8e1a320e2d
Aarona KC <aaronakc99@gmail.com>
2025-09-15 13:02:43 +0545

    feat:make the responsiveness smooth

--- src/features/dashboard/people/pages/PeoplePage.jsx
+++ src/features/dashboard/people/pages/PeoplePage.jsx
@@ -9,6 +9,11 @@ function PeoplePage() {
   return (
     <>
       <Header title="Staffs/Attendees" />
+      <div className="w-full flex justify-center lg:justify-end px-4 sm:px-6 py-2">
+        <p className="underline text-gray-600 text-sm sm:text-base lg:pe-24 xl:pe-28 pe-0">
+          Download Format
+        </p>
+      </div>
       <AddAttendeesCard />
     </>
   )
