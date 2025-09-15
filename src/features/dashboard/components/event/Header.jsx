35f9e94c23d1137d4b76a24f04cff298a7a419de
krishmamaharjan <krishmamaharjan73@gmail.com>
2025-09-15 17:39:47 +0545

    feat: Header Component added

---
+++ src/features/dashboard/components/event/Header.jsx
@@ -0,0 +1,48 @@
+import { FaPlus } from "react-icons/fa";
+import { NavLink } from "react-router-dom";
+
+export default function Header() {
+  const tabs = [
+    { name: "ALL", path: "/events/myallevent" },
+    { name: "PAST", path: "/events/pastevent" },
+    { name: "ON GOING", path: "/events/ongoingevent" },
+    { name: "UPCOMING", path: "/events/upcomingevent" },
+  ];
+
+  return (
+    <div className="">
+      <div className="flex items-center justify-between mt-10 pl-20 pr-20">
+        
+        <div className="flex">
+          {tabs.map((tab) => (
+            <NavLink
+              key={tab.name}
+              to={tab.path}
+              end
+              className={({ isActive }) =>
+                `border px-5 py-1 transition-colors duration-200 ${
+                  isActive ? "bg-sidebar-bg text-white" : "hover:bg-buttonpurple"
+                }`
+              }
+            >
+              {tab.name}
+            </NavLink>
+          ))}
+        </div>
+
+        
+        <div>
+          <span>Event Categories</span>
+        </div>
+
+        
+        <button className="bg-[#772a92] text-white px-4 py-2 rounded-3xl flex items-center gap-2">
+          <FaPlus />
+          CreateEvent
+        </button>
+      </div>
+
+      <hr className="ml-20 mr-20 m-10 border-gray-400 border-2" />
+    </div>
+  );
+}
