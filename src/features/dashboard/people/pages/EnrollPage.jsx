16caacd6b3f41921c08c6536b12f2a0c35a52e9c
Aarona KC <aaronakc99@gmail.com>
2025-09-16 13:02:34 +0545

    feat:add the filter tab

--- src/features/dashboard/people/pages/EnrollPage.jsx
+++ src/features/dashboard/people/pages/EnrollPage.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PeopleScreenHeader from '../components/PeopleScreenHeader'
+import FilterTab from '../components/FilterTab'
 
 function EnrollPage() {
+  const [activeTab, setActiveTab] = useState('Staffs')
   return (
     <>
       <div className='w-full px-20 '>
         <PeopleScreenHeader title='Enrollment' showForm={false} showBtn={false} />
+        <FilterTab filterByTitle1="Staffs" filterByTitle2="Attendees" activeTab={activeTab} setActiveTab={setActiveTab}
+        />
       </div>
     </>
   )
