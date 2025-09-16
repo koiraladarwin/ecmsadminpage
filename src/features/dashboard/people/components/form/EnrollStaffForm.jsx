4a357809c25a86aa234ef7a59cd7ffb70b99c8ad
Aarona KC <aaronakc99@gmail.com>
2025-09-16 16:31:39 +0545

    refactor:use the custom dropdown in staff enroll form

--- src/features/dashboard/people/components/form/EnrollStaffForm.jsx
+++ src/features/dashboard/people/components/form/EnrollStaffForm.jsx
@@ -1,48 +1,65 @@
-import React from 'react'
+import React, { useState } from 'react'
 import NormalBtn from '../NormalBtn'
 import SearchableDropdown from '../SearchableDropdown'
+import CustomDropdown from '../CustomDropDown'
+import Swal from 'sweetalert2'
 
 function EnrollStaffForm() {
   const staffOptions = [
     { id: 'STF-001', name: 'Mr. Carlos Cole' },
     { id: 'STF-002', name: 'Mr. Peter Lee' },
   ]
-  const handleStaffSelect = (staff) => {
+  const eventOptions = ['Event1', 'Event2', 'Event3']
+  const sessionOptions = ['Session1', 'Session2', 'Session3']
 
+  const [formData, setFormData] = useState({
+    attendee: null,
+    event: null,
+    session: null,
+  })
+
+
+  const handleSelect = (key, value) => {
+    setFormData({ ...formData, [key]: value })
   }
+
+  const handleSubmit = () => {
+    if (!formData.attendee || !formData.event || !formData.session) {
+      Swal.fire('Please Fill all the fields!')
+      return
+    }
+  }
+
   return (
     <div className='w-full bg-white px-20 py-10 border-[1.3px] border-b-sidebar-bg '>
       {/* staff and events */}
-      <div className='flex items-center w-full gap-8'>
-        <SearchableDropdown label='Staffs' options={staffOptions} onSelect={handleStaffSelect} />
+      <div className='flex items-end w-full gap-8'>
+        <div className='flex flex-col flex-1'>
+          <SearchableDropdown label='Staffs' options={staffOptions} onSelect={(attendee) => handleSelect('attendee', attendee)} />
+        </div>
+
         {/* events */}
-        <div className='flex flex-col flex-2'>
-          <label className='font-bold text-sidebar-bg'>Events</label>
-          <select className='focus:outline-none' >
-            <option>Select Event</option>
-            <option>Event 1</option>
-            <option>Event 2</option>
-            <option>Event 3</option>
-            <option>Event 4</option>
-            <option>Event 5</option>
-          </select>
+        <div className='flex flex-col flex-1'>
+          <CustomDropdown
+            label='Events'
+            options={eventOptions}
+            onSelect={(value) => handleSelect('event', value)}
+          />
         </div>
       </div>
 
       {/* session and enroll */}
       <div className='flex items-end w-full gap-6 py-4'>
-        <div className='flex flex-col w-[33%]'>
-          <label className='font-bold text-sidebar-bg'>Session</label>
-          <select className='border p-2  focus:outline-none'>
-            <option>Select Session</option>
-            <option>Session 1</option>
-            <option>Session 2</option>
-            <option>Session 3</option>
-          </select>
+        <div className='flex flex-col w-[48%]'>
+          <CustomDropdown
+            label='Session'
+            options={sessionOptions}
+            onSelect={(value) => handleSelect('session', value)}
+          />
         </div>
         {/* btn */}
-        <div>
-          <NormalBtn text='Enroll' type='primary' />
+        <div className='ml-2'>
+          <NormalBtn text='Enroll' type='primary' onClick={handleSubmit} />
         </div>
       </div>
     </div>
