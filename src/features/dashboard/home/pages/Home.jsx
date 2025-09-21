8c82a2805d25b79d8939e1b6298f42327b1cc8cb
Aarona KC <aaronakc99@gmail.com>
2025-09-21 16:24:46 +0545

    feat:add the chart and fix the css

--- src/features/dashboard/home/pages/Home.jsx
+++ src/features/dashboard/home/pages/Home.jsx
@@ -9,11 +9,11 @@ import { MdPersonOutline } from "react-icons/md";
 
 export default function Home() {
   return (
-    <div className="h-full py-4 px-32 flex flex-col space-y-4 justify-center">
+    <div className="h-full w-full py-4 px-10  flex flex-col space-y-6 justify-center pt-360 md:pt-80 ">
       <div className="grid grid-cols-1 md:grid-cols-3 gap-8 flex-2">
-        <StatCard title="ACTIVE EVENTS" value="42%" subtext="21/50" />
-        <StatCard title="ENROLLMENT STATUS" value="30%" subtext="150/500" />
-        <StatCard title="TOTAL ATTENDEES" value="80%" subtext="1568" />
+        <StatCard title="ACTIVE EVENTS" value={21} total={50} subtext="21/50" />
+        <StatCard title="ENROLLMENT STATUS" value={150} total={500} subtext="150/500" />
+        <StatCard title="TOTAL ATTENDEES" value={1600} total={2000} showTotal={true} />
       </div>
 
       <div className="grid grid-cols-1 lg:grid-cols-4 gap-8 flex-3">
@@ -24,8 +24,8 @@ export default function Home() {
 
       <div className="grid grid-cols-1 md:grid-cols-4 gap-8 items-center flex-1">
         <HireBox />
-        <StatBox title="ENROLLED STAFF" value="4" Icon={MdPersonOutline}/>
-        <StatBox title="ACTIVE ISSUES" value="0" Icon={BsPersonVcard}/>
+        <StatBox title="ENROLLED STAFF" value="4" Icon={MdPersonOutline} />
+        <StatBox title="ACTIVE ISSUES" value="0" Icon={BsPersonVcard} />
       </div>
     </div>
   );
