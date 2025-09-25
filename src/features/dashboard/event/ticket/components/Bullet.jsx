2a9758dee0bbfca47963694e48c58d4b9300e0b7
krishmamaharjan <krishmamaharjan73@gmail.com>
2025-09-25 10:15:44 +0545

    feat: create new ticket added

---
+++ src/features/dashboard/event/ticket/components/Bullet.jsx
@@ -0,0 +1,9 @@
+export default function bullet()
+{
+    return(
+        <div className="relative flex items-center">
+            <div className="w-6 bg-buttonpurple border-none rounded-full py-1.5 px-2 "></div>
+            <div className="w-2.5 h-2.5 border-none rounded-full bg-white absolute"></div>
+        </div>
+    )
+}
