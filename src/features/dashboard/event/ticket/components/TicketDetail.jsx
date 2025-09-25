cca946488e2c3cc9be939aa37d91fa169ecc2efd
krishmamaharjan <krishmamaharjan73@gmail.com>
2025-09-25 10:19:14 +0545

    feat: Ticket detail added

---
+++ src/features/dashboard/event/ticket/components/TicketDetail.jsx
@@ -0,0 +1,15 @@
+export const ticket = [
+    {
+        title: "General Invitation",
+        subtitle:"31st cargo Day 2025",
+        startdate: "11 Sep 2025",
+        starttime: "11:00 AM", 
+        enddate:"15 sep 2025",
+        endtime:"05:00 PM",
+        venue:"Master Hall, Hyatt Regency, Kathmandu", 
+        invitation:"150",
+        status:"on sale",
+        price: "Rs.500",
+        sold: "12/50"
+        },
+]
