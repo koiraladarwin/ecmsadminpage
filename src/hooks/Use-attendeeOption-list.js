b2cff186ebbdb82ac39b740c6553657ad99495bc
krishmamaharjan <krishmamaharjan73@gmail.com>
2025-09-26 10:07:27 +0545

    feat: hooks addded for attendee-staff

---
+++ src/hooks/Use-attendeeOption-list.js
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+
+export default function useAttendeeOption() {
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+
+    const fetchEvents = async () => {
+      try {
+        const response = await new Promise((resolve) =>
+          setTimeout(() => {
+            resolve([
+            
+                { id: 'STF-001', name: 'Mr. Carlos Cole' },
+                { id: 'STF-002', name: 'Mr. Peter Lee' },
+                              
+            ]);
+          }, 1000)
+        );
+
+        setEvents(response);
+      } catch (error) {
+        console.error("Error fetching events:", error);
+      }
+    };
+
+    fetchEvents();
+  }, []);
+
+  return events;
+}
+
