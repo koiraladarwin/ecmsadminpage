d7eef6ed36fcf946cf86271d2be51b12b669ebca
krishmamaharjan <krishmamaharjan73@gmail.com>
2025-10-10 12:34:02 +0545

    refac: removed show dropdown-invitations

--- src/features/dashboard/event/invitations/pages/ShowAllInvitations.jsx
+++ src/features/dashboard/event/invitations/pages/ShowAllInvitations.jsx
@@ -7,38 +7,14 @@ import { useState } from "react";
 export default function ShowAllInvitations()
 {
     const invitation = useInvitation();
-    const [open, setOpen] = useState(false);
-    const [selected, setSelected] = useState("All");
 
     return(
         <div className="min-h-screen">
             <div className="flex flex-col md:flex-row gap-4 items-center justify-between pt-10 px-20">
                 <div className="space-y-2">
-                    <div className="flex gap-2">
-                        <h1 className="text-xl">Show</h1>
-
-                        <div className="relative flex items-center justify-center h-full">
-                            <button 
-                                onClick={() => setOpen(!open)}
-                                className=" flex items-center justify-between border rounded-full px-4 py-1 w-40 bg-white">
-                                {selected}
-                                <FaChevronDown className="ml-2 text-buttonpurple text-xs"/>
-                            </button>
-
-                            {open &&(
-                                <div className="absolute top-full right-0 mt-2 w-32 rounded-md  shadow-lg border border-none bg-white text-left">
-                                    <button className="w-full px-4 py-2 hover:bg-gray-100 hover:rounded-md text-left">All</button>
-                                    <button className="w-full px-4 py-2 hover:bg-gray-100 hover:rounded-md text-left">Event wise</button>
-                                </div>
-                            )}
-                        </div>
-                    </div>
-
-
                     <h1 className="text-xl ">Invitations - ALL</h1>
                 </div>
                 
-
                 <NavLink to="/event/createinvitation" >
                 <button className="bg-[#772a92] text-white px-4 py-1 rounded-3xl flex items-center gap-2 text-xl">
                 <FaPlus />
