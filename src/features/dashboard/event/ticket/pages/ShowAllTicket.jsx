f95cfd532a6ad74e1f514677d776908a0ee763e0
krishmamaharjan <krishmamaharjan73@gmail.com>
2025-10-10 12:39:19 +0545

    refac: removed show dropdown-tickets

--- src/features/dashboard/event/ticket/pages/ShowAllTicket.jsx
+++ src/features/dashboard/event/ticket/pages/ShowAllTicket.jsx
@@ -7,34 +7,12 @@ import TicketCard from "../components/TicketCard";
 export default function ShowAllTicket()
 {
     const ticket = useTicket();
-    const [open, setOpen] = useState(false);
-    const [selected, setSelected] = useState("All");
+
 
     return(
         <div className="min-h-screen">
             <div className="flex items-center justify-between pt-10 px-20 flex-col lg:flex-row gap-4">
                 <div className="space-y-2">
-                    <div className="flex gap-2">
-                        <h1 className="text-xl">Show</h1>
-
-                        <div className="relative flex items-center justify-center h-full">
-                            <button 
-                                onClick={() => setOpen(!open)}
-                                className=" flex items-center justify-between border rounded-full px-4 py-1 w-40 bg-white">
-                                {selected}
-                                <FaChevronDown className="ml-2 text-buttonpurple text-xs"/>
-                            </button>
-
-                            {open &&(
-                                <div className="absolute top-full right-0 mt-2 w-32 rounded-md  shadow-lg border border-none bg-white text-left">
-                                    <button className="w-full px-4 py-2 hover:bg-gray-100 hover:rounded-md text-left">All</button>
-                                    <button className="w-full px-4 py-2 hover:bg-gray-100 hover:rounded-md text-left">Event wise</button>
-                                </div>
-                            )}
-                        </div>
-                    </div>
-
-
                     <h1 className="text-xl ">Tickets - ALL</h1>
                 </div>
                 
