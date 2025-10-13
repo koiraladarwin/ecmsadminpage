078548b43e4b7337ab22f4c7e0bbba3b8162359e
Krishma Maharjan <126758112+krishmamaharjan@users.noreply.github.com>
2025-10-13 18:37:19 +0545

    feat: Createevent
    * refac: used api from axios instead axiosinstance from apijs
    
    * check: create event failed
    
    * create event hook
    
    * get events

--- src/features/dashboard/event/components/MyAllEvent.jsx
+++ src/features/dashboard/event/components/MyAllEvent.jsx
@@ -3,7 +3,14 @@ import Header from "./Header.jsx";
 import useEvents from "../../../../hooks/Use-event-list.js";
 export default function MyallEvent()
 {
-    const events = useEvents();
+    // const events = useEvents();
+
+    const {data: events, isLoading, isError} = useEvents();
+
+    if(isLoading) return <p>Loading events...</p>;
+    if(isError) return <p>Failed to load events.</p>;
+    if(!events?.length) return <p>No events Found</p>;
+
     const filteredEvents = events.filter(
         (event) => event.status?.trim().toLowerCase() !== "soon"
     );
