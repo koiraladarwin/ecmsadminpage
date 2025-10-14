2adf7d6c6fa8ff854eb61fb7719ac599ace986c0
Aarona KC <aaronakc99@gmail.com>
2025-10-14 21:44:16 +0545

    feat:show the event with ticket only

--- src/features/dashboard/event/ticket/components/TicketCard.jsx
+++ src/features/dashboard/event/ticket/components/TicketCard.jsx
@@ -7,29 +7,34 @@ export default function TicketCard({
     ticket,
 
 }) {
+    const hasTickets = ticket && ticket.length > 0;
     return (
-        <div className="flex justify-between flex-col gap-2 lg:flex-col box-border p-2 m-2 border-none rounded-lg md:shadow-2xl px-4 py-6  ">
-            <EventDetailCard
-                img={img}
-                name={event.name}
-                start_time={event.start_time}
-                end_time={event.end_time}
-                location={event.location}
-                event_organizer={event.event_organizer}
-            />
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-                {ticket?.map((t) => {
-                    return (
-                        <TicketsCard
-                            key={t.id}
-                            count={t.attendees.length}  
-                            link={`/events/${event.id}/${t.id}`}             
-                            title={t.ticket_category_tag}  
-                            price={t.price}
-                        />
-                    );
-                })}
-            </div>
-        </div>
+        <>
+            {hasTickets && (
+                <div className="flex justify-between flex-col gap-2 lg:flex-col box-border p-2 m-2 border-none rounded-lg md:shadow-2xl px-4 py-6  ">
+                    <EventDetailCard
+                        img={img}
+                        name={event.name}
+                        start_time={event.start_time}
+                        end_time={event.end_time}
+                        location={event.location}
+                        event_organizer={event.event_organizer}
+                    />
+                    <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+                        {ticket?.map((t) => {
+                            return (
+                                <TicketsCard
+                                    key={t.id}
+                                    count={t.attendees.length}
+                                    link={`/events/${event.id}/${t.id}`}
+                                    title={t.ticket_category_tag}
+                                    price={t.price}
+                                />
+                            );
+                        })}
+                    </div>
+                </div>
+            )}
+        </>
     )
 }
