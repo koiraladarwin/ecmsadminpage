d39b2abb21bf4b8d0a4667041de181b96cccdd60
Aarona KC <aaronakc99@gmail.com>
2025-10-16 23:43:47 +0545

    feat:implement the backend api integration for the csv file upload

--- src/features/dashboard/people/components/staff/attendee/PeopleDisplay.jsx
+++ src/features/dashboard/people/components/staff/attendee/PeopleDisplay.jsx
@@ -4,21 +4,141 @@ import PersonListCard from './PersonListCard'
 import { useLocation, useNavigate } from 'react-router-dom'
 import useStaff from '../../../../../../hooks/Use-Staff-list'
 import useAttendee from '../../../../../../hooks/Use-attendee-list'
-import { OrbitProgress } from 'react-loading-indicators'
-import { useEffect } from 'react'
+import useAttendeeCategory from '../../../../../../hooks/Use-attendeeCategory-list'
+import { OrbitProgress, ThreeDot } from 'react-loading-indicators'
+import { useEffect, useState } from 'react'
+import Papa from 'papaparse'
+import Swal from 'sweetalert2'
+import { useMutation } from '@tanstack/react-query'
+import { api, setToken } from '../../../../../../axios/Axios'
+import { useAuth } from '../../../../../auth/context/AuthContext'
+import CsvTable from '../../csvfiledisplay/CsvTable'
 
 const PeopleDisplay = ({ activeTab, setActiveTab }) => {
 
   const { data: totalAttendees, isLoading: totalAttendeesLoading } = useAttendee()
   const { data: totalStaff, isLoading: totalStaffLoading } = useStaff()
+  const { data: attendeeCategory } = useAttendeeCategory()
+  const { firebaseToken } = useAuth()
   const { state } = useLocation()
+  const [csvData, setCsvData] = useState([])
+  const [csvLoading, setCsvLoading] = useState(false)
+  const [showCsvTable, setShowCsvTable] = useState(false)
+  const [search, setSearch] = useState("")
+  const [debouncedSearch, setDebouncedSearch] = useState("")
+  const navigate = useNavigate()
+
+  const dataToShow = activeTab === "staff" ? totalStaff || [] : totalAttendees || []
 
   useEffect(() => {
     if (state) setActiveTab(state)
   }, [state])
 
-  const dataToShow = activeTab === "staff" ? totalStaff : totalAttendees
-  const navigate = useNavigate()
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setDebouncedSearch(search)
+    }, 300);
+
+    return () => clearTimeout(timeout)
+  }, [search])
+
+
+  const mutation = useMutation({
+    mutationFn: (payload) => {
+      const { category, ...data } = payload
+      setToken(firebaseToken);
+      return api.post('/attendee', data);
+    },
+  });
+
+  const { mutateAsync: createAttendee } = mutation;
+
+  const handleCsvFile = async (e) => {
+    try {
+      const file = e.target.files[0];
+      if (!file) return;
+      setCsvData([]);
+
+      Papa.parse(file, {
+        header: true,
+        skipEmptyLines: true,
+        complete: async (results) => {
+          const firstElement = results.data[0];
+          if (
+            !firstElement?.full_name ||
+            !firstElement.phone_number ||
+            !firstElement?.category ||
+            !firstElement?.gmail
+          ) {
+            Swal.fire(
+              "The columns should be named full_name, phone_number, category, gmail, company(if exists), position(if exists)"
+            );
+            return;
+          }
+
+          setCsvLoading(true);
+
+
+          const cleanData = results.data.filter(
+            (row) => row.full_name && row.phone_number && row.category && row.gmail
+          );
+
+
+          for (const data of cleanData) {
+            let { full_name, phone_number, category, gmail, company, position } = data;
+            const cat = attendeeCategory.find((cat) => cat.tag === category);
+
+            full_name = full_name?.trim();
+            phone_number = phone_number?.trim();
+            category = category?.trim();
+            gmail = gmail?.trim();
+            company = company?.trim() || "";
+            position = position?.trim() || "";
+
+            try {
+              await createAttendee({
+                full_name,
+                phone_number,
+                gmail,
+                company,
+                position,
+                attendee_category_id: cat?.id || "",
+                category,
+              });
+
+              setCsvData((prev) => [...prev, { ...data, success: true }]);
+            } catch (err) {
+              console.log("error in csv file upload", err);
+              setCsvData((prev) => [...prev, { ...data, success: false }]);
+            }
+          }
+
+          setCsvLoading(false);
+          setShowCsvTable(true);
+
+          e.target.value = "";
+        },
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  if (showCsvTable) {
+    return (<div>
+      <div>
+        <CsvTable setShow={setShowCsvTable} data={csvData} />
+      </div>
+    </div>)
+  }
+
+  const filteredData = !totalStaffLoading && !totalAttendeesLoading
+    ? dataToShow.filter((person) => {
+      const name = activeTab === "staff" ? person?.name : person?.full_name
+      return name?.toLowerCase().includes(debouncedSearch.toLowerCase())
+    })
+    : []
+
   return (
     <div className='w-full'>
       <div className='w-full flex justify-between'>
@@ -33,15 +153,36 @@ const PeopleDisplay = ({ activeTab, setActiveTab }) => {
               Add Staff/Attendee
             </span>
           </button>
-          <button className={'flex items-center gap-1 px-2 h-[25px] pe-5 font-semibold rounded-full transition text-xs text-black bg-white border border-gray-800 border-solid cursor-pointer'}>
-            <FiPlus size={12} />
-            <p className='hidden md:block'>
-              Import <span className='text-[10px]'>(csv file only)</span>
-            </p>
-          </button>
+          {
+            activeTab === "staff" ? "" :
+              <>
+                <label className={'flex items-center gap-1 px-2 h-[25px] pe-5 font-semibold rounded-full transition text-xs text-black bg-white border border-gray-800 border-solid cursor-pointer'} htmlFor='csv'>
+                  <FiPlus size={12} />
+                  <p className='hidden md:block'>
+                    Import <span className='text-[10px]'>(csv file only)</span>
+                  </p>
+                  <input type="file" accept='.csv' id='csv' className='hidden' onChange={handleCsvFile} />
+                </label>
+              </>
+          }
         </div>
       </div>
-      <div className='w-full bg-white border border-gray-800 border-solid rounded-sm py-8 px-12'>
+      <div className='w-full bg-white border border-gray-800 border-solid rounded-sm py-8 px-12 relative'>
+        {
+          csvLoading && <div className='absolute inset-0 bg-gray-200/25 z-50 flex pt-60 justify-center'>
+            <div>
+              <div className="flex items-center gap-6">
+                <span className="font-bold text-4xl text-transparent bg-clip-text bg-gradient-to-r from-purple-500 via-pink-500 to-indigo-500 animate-pulse scale-105 drop-shadow-md">
+                  Importing CSV files
+                </span>
+                <ThreeDot
+                  color={["#a855f7", "#ec4899", "#6366f1"]}
+                  size="small"
+                />
+              </div>
+            </div>
+          </div>
+        }
         <div className='w-full flex flex-col md:flex-row justify-between border-b border-gray-800 border-solid pb-7 items-center'>
           <div className="relative w-fit">
             <input
@@ -49,6 +190,8 @@ const PeopleDisplay = ({ activeTab, setActiveTab }) => {
               placeholder={`${activeTab === 'staff' ? 'Search for staff' : 'Search for attendee'}`}
               className="rounded-xs focus:outline-none text-md"
               style={{ padding: '2px 15px 2px 10px', border: '1.9px solid rgba(128,128,128,0.3)' }}
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
             />
             <FaSearch className="absolute right-2 top-1/2 -translate-y-1/2 text-gray-400" />
           </div>
@@ -57,9 +200,14 @@ const PeopleDisplay = ({ activeTab, setActiveTab }) => {
           {
             totalAttendeesLoading || totalStaffLoading ? <div className='col-span-full flex justify-center items-center '><OrbitProgress color="#800080" size="medium" /></div> :
               <>
-                {dataToShow?.map((person) => (
-                  <PersonListCard key={person.id} person={person} activeTab={activeTab} />
-                ))}
+                {
+                  filteredData && filteredData.length > 0 ?
+                    filteredData.map((person) => (
+                      <PersonListCard key={person.id} person={person} activeTab={activeTab} />
+                    ))
+                    :
+                    <div className="col-span-full text-center text-2xl text-gray-500 mt-10">No results found.</div>
+                }
               </>
           }
         </div>
--- src/features/dashboard/people/components/staff/attendee/PersonListCard.jsx
+++ src/features/dashboard/people/components/staff/attendee/PersonListCard.jsx
@@ -8,19 +8,19 @@ const PersonListCard = ({ person, activeTab }) => {
   const number = activeTab === "staff" ? person?.phone : person?.phone_number
   return (
     <div className="w-full flex flex-col rounded-xl shadow-md shadow-black/10 pt-5 border border-gray-800/10 border-solid">
-      <div className="flex gap-3 border-b-2 border-gray-800/10 border-solid ps-5 pb-5">
+      <div className="flex flex-col md:flex-row gap-3 border-b-2 border-gray-800/10 border-solid ps-5 pb-5">
         <img
           src={person?.image || '/placeholder.jpg'}
           alt={name}
-          className="w-14 h-14 rounded-full border-2 border-sidebar-hover object-cover"
+          className="w-14 h-14 rounded-full border-2 border-sidebar-hover object-cover "
         />
         <div className="flex flex-col">
           <div className="flex flex-col -space-y-1">
-            <span className="text-sm font-semibold">{name}</span>
+            <span className="text-xs font-semibold break-words">{name}</span>
             <span className="text-gray-800/50 font-semibold text-xs pt-1">{person?.position}</span>
           </div>
-          <span className="text-sm font-medium text-gray-500">{person?.company}</span>
-          <div className="flex flex-col  text-gray-500 text-xs mt-1">
+          <span className="text-xs font-medium text-gray-500">{person?.company}</span>
+          <div className="flex flex-col  text-gray-500 text-xs mt-1 break-words">
             <span>{gmail}</span>
             <span>{number}</span>
           </div>
