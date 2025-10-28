66fe10c6bb61a7adaa60dec90945a650d0c38ac1
krishmamaharjan <krishmamaharjan73@gmail.com>
2025-10-28 18:54:32 +0545

    feat: attendee cards added

---
+++ src/features/dashboard/event/invitations/components/AttendeeCardDetail.jsx
@@ -0,0 +1,40 @@
+import { IoMdEye } from "react-icons/io"
+import { FaWhatsapp } from "react-icons/fa";
+import { CiMail } from "react-icons/ci";
+export default function AttendeeCard({
+    full_name,
+    position,
+    company,
+    image_url,
+    phone_number,
+    gmail,
+}) {
+    return (
+        <div>
+            <div className="border border-textgray rounded-2xl shadow-2xl ">
+                <div className="flex gap-8 m-2 p-2">
+                    <div >
+                        <img src={'/placeholder.jpg'} alt="" className="w-14 h-14 border-2 rounded-full border-buttonpurple" />
+
+                    </div>
+                    <div className="flex flex-col items-start">
+                        <h1>{full_name}</h1>
+                        <p className="text-sm text-textgray">{position}</p>
+                        <p className="text-sm text-textgray">{company}</p>
+                        <p className="text-xs text-textgray">{gmail}</p>
+                        <p className="text-xs text-textgray">{phone_number}</p>
+                    </div>
+                </div>
+
+                <hr className="text-textgray" />
+
+                <div className="flex gap-4 items-center justify-center m-2">
+                    <IoMdEye size={30} color="grey" />
+                    <FaWhatsapp size={24} color="grey" />
+                    <CiMail size={24} color="grey" />
+                </div>
+
+            </div>
+        </div>
+    )
+}
